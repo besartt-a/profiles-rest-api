05f0dd2360b589ec03debd4a4a19be87ecb67b2b
= <=>
2021-05-07 00:32:59 +0200

    Viewset to profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+from rest_framework import viewsets
 
-# Create your views here.
+class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
+    def get(self, request, format=None):
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, put, patch, delete)',
+            'Is traditional to a django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello ' + ''.join(name)
+            return Response({'Message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self,request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self,request, pk=None):
+        return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        a_viewset = [
+            'something written'
+        ]
+
+        return Response(a_viewset)
+
+    def create(self, request):
+
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message='Hello ' + str(name)
+            return Response(message)
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        return Response({'http_method': 'DELETE'})
